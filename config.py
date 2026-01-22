8205d65a5d09af7838d368b88b06a42c5c19c108
Santiago Sack <santiagonsack@gmail.com>
2026-01-22 23:42:49 +0000

    genero congi.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    # base de datos
+    DATABASE_FOLDER = os.path.join(BASE_DIR,'database')
+    DATABASE = os.path.join(DATABASE_FOLDER,'database.db')
+
+    BACKUP_FOLDER = os.path.join(BASE_DIR,'gestor_gastos/backup')
+    BACKUP = os.path.join(BACKUP_FOLDER,)
